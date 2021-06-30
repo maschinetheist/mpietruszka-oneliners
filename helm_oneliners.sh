221bb2a2701d6fb8dd861b8530790db7a2094984
Mike Pietruszka <mike@mpietruszka.com>
2021-06-30 11:13:14 -0500

    Added Helm onliners

---
+++ helm_oneliners.sh
@@ -0,0 +1,20 @@
+# List installed charts
+helm ls -n istio-system
+
+# Add stable repo
+helm repo add stable https://charts.helm.sh/stable
+
+# Helm repo update
+helm repo update
+
+# Install istio from helm release
+helm install istiod manifests/charts/istio-control/istio-discovery -n istio-system
+
+# Search for charts
+helm search hub $chartname
+
+# Install chart from helm
+helm install $releasename $chartname -n $namespace
+
+# Get status of a chart
+helm status istio-base -n istio-system
